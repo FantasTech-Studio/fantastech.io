d66b161f85198f4571d9489c689aed8fdf5b7885
LucaBia <gian.luca.99@hotmail.com>
2024-08-21 18:59:55 +0200

    feat: footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import About from "@/components/About";
 import { Contact } from "@/components/Contact";
+import { Footer } from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 import { FloatingNav } from "@/components/ui/FloatingNav";
@@ -20,6 +21,7 @@ export default function Home() {
         <About />
         <Services />
         <Contact />
+        <Footer />
       </div>
     </main>
   );
