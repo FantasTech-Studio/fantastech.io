a9ad9c952197df0f73d6fbcaef93ff96b8e401ec
LucaBia <gian.luca.99@hotmail.com>
2025-04-16 17:13:52 -0600

    feat: contact and footer translate

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -6,55 +6,60 @@ import createGlobe from "cobe";
 import { useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { useLanguage } from '@/context/LanguageContext';
 
 
 
 export function FeaturesSectionDemo() {
+    const { t, isLoading } = useLanguage();
+
     const features = [
       {
-        title: "Contáctanos",
-        description:
-          "Tu próximo gran proyecto empieza con una conversación. ¡Hablemos!",
+        title: t('contact.contact_title'),
+        description: t('contact.contact_description'),
         skeleton: <SkeletonOne />,
-        className:
-          "col-span-1 lg:col-span-3 lg:border-r  dark:border-neutral-800",
+        className: "col-span-1 lg:col-span-3 lg:border-r dark:border-neutral-800",
       },
       {
-        title: "Trabaja con nosotros",
-        description:
-          "Únete a nuestro equipo y lleva tu carrera al siguiente nivel. ¡Consulta nuestras posiciones abiertas!",
+        title: t('contact.wwu_title'),
+        description: t('contact.wwu_description'),
         skeleton: <SkeletonTwo />,
         className: "col-span-1 lg:col-span-3 border-b lg:border-none",
       },
     ];
+
+    if (isLoading) {
+      return <div className="py-10 text-center">Loading...</div>;
+    }
+
     return (
       <div className="relative z-20 py-10 lg:py-40 max-w-7xl mx-auto ">
         <div className="px-8">
           <h4 className="text-3xl lg:text-5xl lg:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium text-black dark:text-white">
-                Tecnología sin límites para impulsar tu futuro
+            {t('contact.title')}
           </h4>
   
-          <p className="text-sm lg:text-base  max-w-2xl  my-4 mx-auto text-neutral-500 text-center font-normal dark:text-neutral-300">
-            Desde la idea hasta la ejecución, en Fantastech convertimos tu visión en soluciones tecnológicas innovadoras que impulsan el crecimiento de tu negocio.
+          <p className="text-sm lg:text-base max-w-2xl my-4 mx-auto text-neutral-500 text-center font-normal dark:text-neutral-300">
+            {t('contact.subtitle')}
           </p>
         </div>
   
-        <div className="relative ">
+        <div className="relative">
           <div className="grid grid-cols-1 lg:grid-cols-6 mt-12 xl:border rounded-md dark:border-neutral-800">
             {features.map((feature) => (
               <FeatureCard key={feature.title} className={feature.className}>
                 <FeatureTitle>{feature.title}</FeatureTitle>
                 <FeatureDescription>{feature.description}</FeatureDescription>
-                <div className=" h-full w-full">{feature.skeleton}</div>
+                <div className="h-full w-full">{feature.skeleton}</div>
               </FeatureCard>
             ))}
           </div>
         </div>
       </div>
     );
-  }
-  
-  const FeatureCard = ({
+}
+
+const FeatureCard = ({
     children,
     className,
   }: {
@@ -93,38 +98,40 @@ export function FeaturesSectionDemo() {
 
   
   export const SkeletonOne = () => {
+    const { t } = useLanguage();
     return (
         <div className="w-full mx-auto bg-transparent dark:bg-transparent group h-full">
             <div className="flex flex-1 w-full h-full flex-col space-y-2 relative mt-4 lg:items-start items-center">
                 <Link href="https://cal.com/fantastech" target="__blank">
-                    <button className=" px-8 py-2 lg:py-4 rounded-lg relative bg-slate-700 text-white hover:shadow-2xl hover:shadow-white/[0.1] transition duration-200 border border-slate-600 hover:scale-105 mb-3">
-                        <div className="absolute inset-x-0 h-px w-1/2 mx-auto -top-px shadow-2xl  bg-gradient-to-r from-transparent via-teal-500 to-transparent" />
+                    <button className="px-8 py-2 lg:py-4 rounded-lg relative bg-slate-700 text-white hover:shadow-2xl hover:shadow-white/[0.1] transition duration-200 border border-slate-600 hover:scale-105 mb-3">
+                        <div className="absolute inset-x-0 h-px w-1/2 mx-auto -top-px shadow-2xl bg-gradient-to-r from-transparent via-teal-500 to-transparent" />
                         <span className="relative z-20">
-                            Agenda una reunión
+                            {t('contact.contact_cta')}
                         </span>
                     </button>
                 </Link>
                 <Link href="https://wa.me/50223117721" target="__blank">
                     <button className="px-[4.3rem] py-2 lg:py-4 rounded-lg bg-[#1ED760] text-white transform hover:scale-105 hover:bg-[#21e065] transition-colors duration-200">
-                        WhatsApp
+                        {t('contact.contact_cta2')}
                     </button>
                 </Link>
             </div>
         </div>
     );
-  };
-  
-  export const SkeletonTwo = () => {
+};
+
+export const SkeletonTwo = () => {
+    const { t } = useLanguage();
     return (
-      <div className="h-60 md:h-60  flex flex-col items-center lg:items-start relative bg-transparent dark:bg-transparent mt-4">
+      <div className="h-60 md:h-60 flex flex-col items-center lg:items-start relative bg-transparent dark:bg-transparent mt-4">
         <Link href="https://doc.clickup.com/9006076676/d/h/8ccvmr4-2417/eebc811d1ea7802" target="__blank">
-            <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-lg p-px text-xs font-semibold leading-6  text-white inline-block">
+            <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-lg p-px text-xs font-semibold leading-6 text-white inline-block">
                 <span className="absolute inset-0 overflow-hidden rounded-lg">
                     <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
                 </span>
-                <div className="relative flex space-x-2 items-center z-10 rounded-lg bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+                <div className="relative flex space-x-2 items-center z-10 rounded-lg bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10">
                     <span>
-                        Ver posiciones abiertas
+                        {t('contact.wwu_cta')}
                     </span>
                     <svg
                         fill="none"
@@ -149,7 +156,7 @@ export function FeaturesSectionDemo() {
         <Globe className="absolute -right-40 md:-right-40 -bottom-80 md:-bottom-72" />
       </div>
     );
-  };
+};
   
   export const Globe = ({ className }: { className?: string }) => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
