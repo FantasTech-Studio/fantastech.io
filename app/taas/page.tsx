d8cc36c6828464a6c132b9ce39a87ff2500ab2b8
LucaBia <gian.luca.99@hotmail.com>
2025-05-12 01:12:42 +0200

    feat: why choose us taas

--- app/taas/page.tsx
+++ app/taas/page.tsx
@@ -3,6 +3,7 @@ import { Navbar } from "@/components/ui/Navbar";
 import { Footer } from "@/components/Footer";
 import { useLanguage } from '@/context/LanguageContext';
 import { PlayfulHeroSection } from "@/components/ui/taas/PlayfulHeroSection";
+import { FeaturesSectionDemo } from "@/components/ui/taas/WhyUs";
 
 export default function Taas() {
   const { t, isLoading } = useLanguage();
@@ -12,10 +13,11 @@ export default function Taas() {
   }
 
   return (
-    <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto w-full">
+    <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto w-full dark:bg-black-100">
       <div className="w-full">
         <Navbar />
         <PlayfulHeroSection />
+        <FeaturesSectionDemo />
         <Footer />
       </div>
     </main>
